2bc80c74850f732d1a8af9b692c8a876ee9a676f
karan <karan372003@gmail.com>
2025-06-30 18:38:00 +0530

    database connection done, user model created

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
 const app = express();
 const port = process.env.PORT || 5000
+connectDB()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({credentials:true}))
 
 app.get("/api/status", (req,res)=>res.send("server is live"))
+
 
 
 app.listen(port, ()=>{
